c73aaa73c8420a69a42ad850e1ea8393c3b47b44
Shimnithlal Ahammed <79171113+twoton-tdr@users.noreply.github.com>
2026-01-17 21:42:35 +0530

    character done

--- src/main/java/org/example/asteroid/model/Character.java
+++ src/main/java/org/example/asteroid/model/Character.java
@@ -2,15 +2,76 @@ package org.example.asteroid.model;
 
 import javafx.geometry.Point2D;
 import javafx.scene.shape.Polygon;
+import javafx.scene.shape.Shape;
+import org.example.asteroid.AsteroidApplication;
 
 public abstract class Character {
     private Polygon character;
     private Point2D movement;
 
-    public Character(Polygon poly,int x,int y){
+    public Character(Polygon poly, int x, int y) {
         character = poly;
         character.setTranslateX(x);
         character.setTranslateY(y);
         movement = new Point2D(0, 0);
+    }
+
+    public Polygon getCharecter() {
+        return character;
+    }
+
+    public void turnleft() {
+        character.setRotate(character.getRotate() - 5);
+    }
+
+    public void turnRight() {
+        character.setRotate(character.getRotate() + 5);
+    }
+
+    public void move() {
+        character.setTranslateX(character.getTranslateX() + movement.getX());
+        character.setTranslateY(character.getTranslateY() + movement.getY());
+
+        if (this.character.getTranslateX() < -20) {
+            this.character.setTranslateX(this.character.getTranslateX() + AsteroidApplication.WIDHT);
+        }
+
+        if (character.getTranslateX() > AsteroidApplication.WIDHT + 20) {
+            character.setTranslateX(this.character.getTranslateX() % AsteroidApplication.WIDHT);
+        }
+
+        if (this.character.getTranslateY() > AsteroidApplication.HEIGHT + 20) {
+            this.character.setTranslateY(this.character.getTranslateY() % AsteroidApplication.HEIGHT);
+        }
+
+        if (this.character.getTranslateY() < -20) {
+            this.character.setTranslateY(this.character.getTranslateY() + AsteroidApplication.HEIGHT);
+        }
+    }
+
+    public Point2D getMovement() {
+        return movement;
+    }
+
+    public void setMovement(Point2D movement) {
+        this.movement = movement;
+    }
+
+    public boolean collide(Character other) {
+        Shape collisionArea = Shape.intersect(this.character, other.getCharecter());
+        return collisionArea.getBoundsInLocal().getWidth() != -1;
+
+    }
+
+    public void accelerate() {
+        double angle = Math.toRadians(this.character.getRotate());
+        double changeX = Math.cos(angle);
+        double changeY = Math.sin(angle);
+
+        changeX *= 0.05;
+        changeY *= 0.05;
+
+        this.movement = this.movement.add(changeX, changeY);
+
     }
 }
